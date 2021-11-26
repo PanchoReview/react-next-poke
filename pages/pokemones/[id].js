79c5ab4e18cc3b885a3e2cae26955f5fb0eb7374
Francisco Astorga <fastorga@nimok.com>
2021-11-26 14:33:57 -0300

    Implementado getStaticPaths en [id].js
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,9 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-const Pokemon = ( { data } ) => {
-    console.log(data)    
-    const id = data.id;
+const Pokemon = ( { data } ) => {    
+    const router = useRouter()
+    console.log(router)    
+
+    if (router.isFallback) {
+        return <p>Cargando...</p>
+    }
+
     return(
         <div>
             <h1>{data.name} numero #{data.id}</h1>
@@ -15,10 +21,39 @@ const Pokemon = ( { data } ) => {
 
 export default Pokemon
 
-export const getServerSideProps = async ({params}) => {
+export const getStaticProps = async ({params}) => {
     console.log(params)
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
     const data = await response.json()
 
     return { props: { data } }
 }
+
+//Definir rutas estáticas para las que next generará HTMLs estáticos
+export const getStaticPaths = async ({ }) => {
+    const paths = [
+        { params: { id: '1'} },
+        { params: { id: '2'} }
+    ]
+    
+
+    //se retornan:
+    //paths
+    //fallback: si es true, rutas no definidas en paths se generarán on demand
+    //fallback es util cuando tenemos una página con potenciales miles de rutas estáticas
+    //fallback acepta valores true, false y 'blocking'
+    //'blocking' y true son similares, pero con 'blocking' no se puede controlar el comportamiento
+    //del componente mientras se encuentra generando
+    return {
+        paths,
+        fallback: 'blocking' 
+    }
+}
+
+// export const getServerSideProps = async ({params}) => {
+//     console.log(params)
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+//     const data = await response.json()
+
+//     return { props: { data } }
+// }
