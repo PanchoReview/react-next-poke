a90584ddca1241263fb131ee70e26a0ac0a5ef41
Francisco Astorga <fastorga@nimok.com>
2021-11-26 16:30:16 -0300

    Evaluacion de componentes dinamicos en index.test.js
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,22 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Index from '../pages/index'
+
+const pokemones = [ { name: 'Chanchito feliz', url: '/pokemon/detalle/1' } ]
 
 describe('Index', () => {
     describe('Component', () => {
         it('renders', () => {
-            const { getByTestId } = render(<Index pokemones={[]} />)
+            const { getByTestId } = render(<Index pokemones={pokemones} />)
 
             const paragraph = getByTestId("titulo")
             expect(paragraph).toBeInTheDocument()
+
+            const chanchito = screen.getByText('Chanchito feliz')
+            expect(chanchito).toBeInTheDocument()
+
+            const url = chanchito.getAttribute("href")            
+            expect(url).toEqual("/pokemones/1")
+
         })
 
         
